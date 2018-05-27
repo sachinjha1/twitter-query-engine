3f236da2123a0c41c4e2e0ab9d69609c535e8830
Sachin Jha <Sachin.Jha@walmart.com>
2018-05-27 13:02:33 -0500

    Tweet Badge UX improved

--- src/client/components/tweets-search/tweet-query.jsx
+++ src/client/components/tweets-search/tweet-query.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import TextField from "material-ui/TextField";
-import Button from "material-ui/Button";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const TweetQuery = ({_setTweetsQueryField, _setTweetsQueryOperator, _setTweetsQueryValue, _searchTweets, _query}) => (
   <div>
--- src/client/components/tweets-search/tweet.jsx
+++ src/client/components/tweets-search/tweet.jsx
@@ -1,13 +1,68 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Typography } from 'material-ui';
+import { withStyles } from '@material-ui/core/styles';
+import { Card, CardHeader, Typography, Avatar, Grid, Badge, Tooltip } from '@material-ui/core';
+import CachedIcon from '@material-ui/icons/Cached';
+import LanguageIcon from '@material-ui/icons/Language';
+import VerifiedIcon from '@material-ui/icons/VerifiedUser';
 
-const Tweet = ({ tweet }) => (
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  margin: {
+    margin: 4,
+  },
+});
+
+let userIds = ['AB','BC','CD','DE','EF','GH','IJ','JK'];
+
+const Tweet = ({ tweet, classes }) => (
   <div>
-    <Card style={{ maxWidth: 275 }}>
-      <Typography type="headline">
-        <strong>{`${tweet.tweet}-${tweet.user}`}</strong>
-      </Typography>
+    <Card className={ classes.card }>
+      <Grid container wrap="nowrap" spacing={16}>
+
+        <Grid item xs={6}>
+          <CardHeader
+            avatar={
+              <Avatar>
+                {userIds[Math.floor(Math.random()*userIds.length)]}
+              </Avatar>
+            }
+            title='@user-32'
+            subheader={fromTime(tweet.created_at)}/>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Typography type="Subheading">
+            {tweet.tweet.slice(0,tweet.tweet.indexOf('#'))}
+          </Typography>
+          <Typography type="Headline" color='error'>
+            <strong>{tweet.tweet.slice(tweet.tweet.indexOf('#'),tweet.tweet.length)}</strong>
+          </Typography>
+        </Grid>
+
+      </Grid>
+      <Grid container wrap="nowrap" spacing={16}>
+        <Grid item xs={6}/>
+        <Grid item xs={6}>
+          <Tooltip title='Retweet' placement="top">
+            <Badge className={classes.margin} badgeContent={tweet.retweet_count} color="default">
+              <CachedIcon color='disabled'/>
+            </Badge>
+          </Tooltip>
+          <Tooltip title='Language' placement="top">
+            <Badge className={classes.margin} badgeContent={tweet.lang} color="default">
+              <LanguageIcon color='disabled'/>
+            </Badge>
+          </Tooltip>
+          <Tooltip title='Verified' placement="top">
+            <Badge className={classes.margin} badgeContent={''} color="default">
+             {tweet.verified === true?<VerifiedIcon color='disabled'/>:''}
+            </Badge>
+          </Tooltip>
+        </Grid>
+      </Grid>
     </Card>
     <br />
   </div>
@@ -20,4 +75,39 @@ Tweet.propTypes = {
   }).isRequired,
 };
 
-export default Tweet;
+function fromTime(previous) {
+  let current = new Date();
+  let msPerMinute = 60 * 1000;
+  let msPerHour = msPerMinute * 60;
+  let msPerDay = msPerHour * 24;
+  let msPerMonth = msPerDay * 30;
+  let msPerYear = msPerDay * 365;
+
+  let elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    return Math.round(elapsed/1000) + ' sec';
+  }
+
+  else if (elapsed < msPerHour) {
+    return Math.round(elapsed/msPerMinute) + ' min';
+  }
+
+  else if (elapsed < msPerDay ) {
+    return Math.round(elapsed/msPerHour ) + ' hr';
+  }
+
+  else if (elapsed < msPerMonth) {
+    return 'approximately ' + Math.round(elapsed/msPerDay) + ' d';
+  }
+
+  else if (elapsed < msPerYear) {
+    return 'approximately ' + Math.round(elapsed/msPerMonth) + ' mo';
+  }
+
+  else {
+    return 'approximately ' + Math.round(elapsed/msPerYear ) + ' yr';
+  }
+}
+
+export default withStyles(styles)(Tweet);
